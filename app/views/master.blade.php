7c785537af5160e8dd8ae41e77fa9c904e6660bd
张文浩 <zhangwenhao@ganji.com>
2015-04-05 18:03:24 +0800

    add scripts and styles files

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <title>豆瓣图书排名工具</title>
-    <link rel="stylesheet" href="{{asset('bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{asset('bootstrap-table.css')}}">
-    <link rel="stylesheet" href="{{asset('bootstrap-editable.css')}}">
-    {{ HTML::script('jquery.min.js') }}
-    {{ HTML::script('bootstrap.min.js') }}
-    {{ HTML::script('bootstrap-table.js') }}
-    {{ HTML::script('bootstrap-editable.min.js') }}
+    <link rel="stylesheet" href="{{asset('styles/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('styles/bootstrap-table.css')}}">
+    <link rel="stylesheet" href="{{asset('styles/bootstrap-editable.css')}}">
+    {{ HTML::script('scripts/jquery.min.js') }}
+    {{ HTML::script('scripts/bootstrap.min.js') }}
+    {{ HTML::script('scripts/bootstrap-table.js') }}
+    {{ HTML::script('scripts/bootstrap-editable.min.js') }}
 </head>
 <body>
 <div class="container">
